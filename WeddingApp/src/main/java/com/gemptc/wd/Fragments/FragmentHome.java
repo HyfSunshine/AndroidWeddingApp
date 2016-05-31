1955c651e4fecd1e8696c9e6a73dd0dec9917a21
Administrator <775183940@qq.com>
2016-05-31 16:58:32 +0800

    1.更新图片存储位置2.写了两个工具类：PrefUtils和UrlAddress3.增加启动界面和登录注册页面（注意，第一次进入的时候false）4.修改了首页的轮播图片5.xutils的Application已经在系统中注册

--- WeddingApp/src/main/java/com/gemptc/wd/Fragments/FragmentHome.java
+++ WeddingApp/src/main/java/com/gemptc/wd/Fragments/FragmentHome.java
@@ -5,6 +5,7 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,120 +13,89 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.android.wedding.R;
-import com.gemptc.wd.adapter.LuoBoViewPagerAdapter;
+import com.gemptc.wd.utils.UrlAddress;
+import com.viewpagerindicator.PageIndicator;
+
+import org.xutils.x;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+
 
 /**
  * Created by Administrator on 2016/5/5.
  */
-public class FragmentHome extends Fragment {
-    private static final int INT = 0;
-    private ViewPager mViewPaper;
-    private List<ImageView> images;
-    private List<View> dots;
-    private int currentItem;
-    //记录上一次点的位置
-    private int oldPosition = 0;
-    //存放图片的id
-    private int[] imageIds = new int[]{
-            R.mipmap.home_luoboa,
-            R.mipmap.home_luoboac,
-            R.mipmap.home_luobob,
-            R.mipmap.home_luobod,
-    };
-    private LuoBoViewPagerAdapter adapter;
-    private ScheduledExecutorService scheduledExecutorService;
+public class FragmentHome extends Fragment{
+    private ViewPager viewPager;
+    private PageIndicator indicator;
+    private List<String> imagesUrlList;
+    private Handler handler;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, null);
-        mViewPaper = (ViewPager) view.findViewById(R.id.home_lunbo_viewpager);
-        //显示的图片
-        images = new ArrayList<ImageView>();
-        for(int i = 0; i < imageIds.length; i++){
-            ImageView imageView = new ImageView(getContext());
-            imageView.setBackgroundResource(imageIds[i]);
-            images.add(imageView);
-        }
-        //显示的小点
-        dots = new ArrayList<>();
-        dots.add(view.findViewById(R.id.dot_0));
-        dots.add(view.findViewById(R.id.dot_1));
-        dots.add(view.findViewById(R.id.dot_2));
-        dots.add(view.findViewById(R.id.dot_3));
-        adapter = new LuoBoViewPagerAdapter(images);
-        mViewPaper.setAdapter(adapter);
-        mViewPaper.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageSelected(int position) {
-                dots.get(position).setBackgroundResource(R.mipmap.dot_focused);
-                dots.get(oldPosition).setBackgroundResource(R.mipmap.dot_normal);
-                oldPosition = position;
-                currentItem = position;
-            }
-            @Override
-            public void onPageScrolled(int arg0, float arg1, int arg2) {
-            }
-            @Override
-            public void onPageScrollStateChanged(int arg0) {
-            }
-        });
-        return  view;
-    }
-    /**
-     * 利用线程池定时执行动画轮播
-     * command：执行线程
-     * initialDelay：初始化延时
-     * period：前一次执行结束到下一次执行开始的间隔时间（间隔执行延迟时间）
-     * unit：计时单位
-     */
-    @Override
-    public void onStart() {
-        // TODO Auto-generated method stub
-        super.onStart();
-        scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
-        scheduledExecutorService.scheduleWithFixedDelay(
-                new ViewPageTask(),
-                2,
-                2,
-                TimeUnit.SECONDS);
-    }
-    /*图片轮播任务*/
-    private class ViewPageTask implements Runnable{
-        @Override
-        public void run() {
-            currentItem = (currentItem + 1) % imageIds.length;
-            mHandler.sendEmptyMessage(INT);
-        }
-    }
-    /**
-     * 接收子线程传递过来的数据
-     */
-    private Handler mHandler = new Handler(){
-        @Override
-        public void handleMessage(Message msg)
-        {
-            int what=msg.what;
-            switch (what){
-                case  INT:
-                    mViewPaper.setCurrentItem(currentItem,false);
-            }
+        View view=inflater.inflate(R.layout.fragment_home,null);
+        viewPager= (ViewPager) view.findViewById(R.id.home_lunbo_viewpager);
+        indicator= (PageIndicator) view.findViewById(R.id.indicator);
 
+        //初始化图片地址
+        initImagesURL();
+
+        viewPager.setAdapter(new MyViewPagerAdapter());
+        indicator.setViewPager(viewPager);
+
+        //自动轮播
+        handler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what==0){
+                    int currentPosition = viewPager.getCurrentItem();
+                    if (currentPosition<imagesUrlList.size()-1){
+                        currentPosition++;
+                    }else{
+                        currentPosition=0;
+                    }
+                    viewPager.setCurrentItem(currentPosition);
+                    handler.sendEmptyMessageDelayed(0,3000);
+                }
+            }
+        };
+
+        handler.sendEmptyMessageDelayed(0,3000);
+        return view;
+    }
+
+    private void initImagesURL() {
+        imagesUrlList=new ArrayList<>();
+        imagesUrlList.add(UrlAddress.LOGIN_IMAGE_ADDRESS+"photo1.jpg");
+        imagesUrlList.add(UrlAddress.LOGIN_IMAGE_ADDRESS+"photo2.png");
+        imagesUrlList.add(UrlAddress.LOGIN_IMAGE_ADDRESS+"photo3.jpg");
+    }
+
+    class MyViewPagerAdapter extends PagerAdapter{
+
+        @Override
+        public int getCount() {
+            return imagesUrlList.size();
         }
 
-    };
-    @Override
-    public void onStop() {
-        // TODO Auto-generated method stub
-        super.onStop();
-        if(scheduledExecutorService != null){
-            scheduledExecutorService.shutdown();
-            scheduledExecutorService = null;
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view==object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            ImageView image = new ImageView(getContext());
+            image.setScaleType(ImageView.ScaleType.FIT_XY);
+            x.image().bind(image,imagesUrlList.get(position));
+            container.addView(image);
+            return image;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
         }
     }
 }
---
+++ WeddingApp/src/main/java/com/gemptc/wd/utils/UrlAddress.java
@@ -0,0 +1,28 @@
+package com.gemptc.wd.utils;
+
+/**
+ * Created by Administrator on 2016/5/31.
+ */
+public class UrlAddress {
+    /*
+	 * 本地使用
+	 */
+    public static final String HOST_ADDRESS="http://10.201.1.9:8080/";
+
+    //public static final String HOST_ADDRESS="http://localhost:8080/";
+    public static final String FILE_SAVE_ADDRESS="G:/JavaProject/WeddingProject/images/";
+
+	/*
+	 * 服务器上使用
+	 */
+    //public static final String HOST_ADDRESS="http://104.224.132.169:8080/";
+    //public static final String FILE_SAVE_ADDRESS="/var/MyProject/images/";
+
+    public static final String HOST_ADDRESS_PROJECT=HOST_ADDRESS+"WeddingJson/";
+
+    public static final String USER_IMAGE_ADDRESS=HOST_ADDRESS+"images/userImages/";
+    public static final String SELLER_IMAGE_ADDRESS=HOST_ADDRESS+"images/sellerImages/";
+    public static final String PRODUCT_IMAGE_ADDRESS=HOST_ADDRESS+"images/productImages/";
+    public static final String POST_IMAGE_ADDRESS=HOST_ADDRESS+"images/postImages/";
+    public static final String LOGIN_IMAGE_ADDRESS=HOST_ADDRESS+"images/loginAndRegisterImages/";
+}
